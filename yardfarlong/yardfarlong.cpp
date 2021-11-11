6cd002b0bfe2a990268818d276119dcde5555c0a
Good <Good@GOOD->
2021-11-11 20:39:06 +0300

    Добавьте файлы проекта.

---
+++ yardfarlong/yardfarlong.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double furlong, yard;
+    cout << "Введите количество фарлонгов: " << endl;
+    cin >> furlong;
+    yard = furlong * 220;
+    cout << yard << " ярдов в данном количестве фарлонгов" << endl;
+}
